b793529ef9831ea4828694ca634880cac281d1a9
tgl89 <120042475+tgl89@users.noreply.github.com>
2022-12-07 09:39:37 -0700

    Add files via upload

---
+++ Get Disk Inventory test script.ps1
@@ -0,0 +1,12 @@
+#get disk space on a remote computer
+param (
+    $computername = 'localhost'
+)
+Write-Host "Analyzing Disk Space"
+Get-WmiObject -Class Win32_logicalDisk -ComputerName $computername -Filter "drivetype=3" | 
+Sort-Object -Property DeviceID | 
+Format-Table -Property DeviceID, 
+@{label='UsedSpace(MB)';expression={$_.Size / 1MB - $_.FreeSpace / 1MB -as [int]}},
+@{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
+@{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+@{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
